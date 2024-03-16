caf32e2664217a53b62401a73bff919eeca7089b
Anthony <anthony089@outlook.fr>
2024-03-16 19:18:52 +0100

    Refactoring deleteSetup command

--- commands/discord/deletesetup.js
+++ commands/discord/deletesetup.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { embedBuilder } = require("./../../utils/embedBuilder");
+const { handleInteractionWithRoles } = require("./../../utils/logging/logger")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,28 +7,16 @@ module.exports = {
         .setDescription('Commande pour créer un channel de cast !'),
     async execute(interaction) {
         try {
-            const guild = interaction.guild;
-            const user = interaction.user;
+            await interaction.deferReply();
+
             const CHANNEL_CATEGORY_TYPE = 4;
 
-            const member = await guild.members.fetch(user.id);
-            const channel = await guild.channels.cache.get(process.env.CHANNEL_ID_LOG_BOT);
-
-            embedBuilder("Log O.R.C.A", member, channel, interaction.commandName);
-
-            if (!member.roles.cache.has(process.env.ROLE_ID_ADMIN)) {
-                interaction.reply({ content: `Vous n'avez pas les permissions requises à l'utilisation de cette commande.`, ephemeral: false });
-                return;
-            }
-
-            if (!guild) {
-                return await interaction.reply({ content: `Problème avec la commande, la guilde n'a pas été trouvée`, ephemeral: false });
-            }
+            await handleInteractionWithRoles(interaction, [process.env.ROLE_ID_ADMIN]);
 
             //Check for a pattern Division followed by a numeric value
             const targetPattern = /^Division \d+$/;
 
-            const divisionCategories = guild.channels.cache.filter(channel =>
+            const divisionCategories = interaction.guild.channels.cache.filter(channel =>
                 channel.type === CHANNEL_CATEGORY_TYPE && targetPattern.test(channel.name)
             );
 
@@ -42,12 +30,9 @@ module.exports = {
                 category.delete();
             });
 
-            interaction.reply({ content: `Les divisions de la saison ont bien été supprimée.` });
-
-
+            interaction.editReply({ content: `Les divisions de la saison ont bien été supprimée.`, ephemeral: false });
         } catch (error) {
-            console.error(error);
-            interaction.reply({ content: `Une erreur s'est produite lors de l'exécution de la commande : ${error}`, ephemeral: false });
+            interaction.editReply({ content: `${error}`, ephemeral: false });
         }
     },
 };
